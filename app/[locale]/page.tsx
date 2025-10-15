dd51af6709afa5db1ac1048af4620e34ba0f05a7
Amer Mushtaq <amer.micromerger@gmail.com>
2025-10-15 15:21:25 +0500

    Fix: trigger Vercel with author info updated

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -4,6 +4,23 @@ import AnimationArea from "@/components/layout/home/AnimationArea";
 import BlogCarousel from "@/components/layout/home/BlogCarousel";
 import ContactSection from "@/components/layout/home/ContactSection";
 
+export default async function HomePage({
+  params,
+}: {
+  params: Promise<{ locale: string }>;
+}) {
+  const { locale } = await params;
+  return (
+    <>
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <AnimationArea />
+        <BlogCarousel locale={locale} />
+        <ContactSection />
+      </main>
+    </>
+  );
+}
+
 // interface LocalePageProps {
 //   params: { locale: string };
 // }
@@ -18,21 +35,6 @@ import ContactSection from "@/components/layout/home/ContactSection";
     </>
   );
 } */
-
-export default async function HomePage({ params }: { params: Promise<{ locale: string }> }) {
-
-  const { locale } = await params;
-  return (
-    <>
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">        
-        <AnimationArea />
-        <BlogCarousel locale={locale} />
-        <ContactSection />
-      </main>
-    </>
-  );
-}
-
 /* 
 
 import type { Metadata } from 'next';
