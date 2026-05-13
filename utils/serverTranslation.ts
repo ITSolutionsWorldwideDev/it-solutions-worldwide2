c10e899837011941c5f064f2282402aa52ef96fb
Mahvia <mahviasajjadabbasi@gmail.com>
2026-05-13 15:57:37 +0500

    removing error

--- utils/serverTranslation.ts
+++ utils/serverTranslation.ts
@@ -1,51 +1,22 @@
 // utils/serverTranslation.ts
-import i18next from 'i18next';
-import Backend from 'i18next-fs-backend';
-import path from 'path';
+import enCommon from "../public/locales/en/common.json";
+import nlCommon from "../public/locales/nl/common.json";
+import i18next from "i18next";
 
-const initServerI18n = async (locale: string, namespaces: string[] = ['common']) => {
-  const i18nInstance = i18next.createInstance();
-
-  await i18nInstance
-    .use(Backend)
-    .init({
-      lng: locale,
-      fallbackLng: 'en',
-      ns: namespaces,
-      defaultNS: 'common',
-      backend: {
-        loadPath: path.resolve(process.cwd(), 'i18n/locales/{{lng}}/{{ns}}.json'),
-      },
-      interpolation: {
-        escapeValue: false,
-      },
-    });
-
-  return i18nInstance;
+const resources = {
+  en: { common: enCommon },
+  nl: { common: nlCommon },
 };
 
-export default initServerI18n;
-
-
-// // utils/serverTranslation.ts
-// import enCommon from "../public/locales/en/common.json";
-// import nlCommon from "../public/locales/nl/common.json";
-// import i18next from "i18next";
-
-// const resources = {
-//   en: { common: enCommon },
-//   nl: { common: nlCommon },
-// };
-
-// export default async function initServerI18n(locale: string) {
-//   const i18nInstance = i18next.createInstance();
-//   await i18nInstance.init({
-//     lng: locale,
-//     fallbackLng: "en",
-//     resources,
-//   });
-//   return i18nInstance;
-// }
+export default async function initServerI18n(locale: string) {
+  const i18nInstance = i18next.createInstance();
+  await i18nInstance.init({
+    lng: locale,
+    fallbackLng: "en",
+    resources,
+  });
+  return i18nInstance;
+}
 
 
 
