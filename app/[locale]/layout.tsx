2dff8ef7030194dd6303b8da1546e567c2d41041
unknown <ahmed962456@gmail.com>
2026-06-10 10:46:17 +0500

    changes by ahmed nadeem

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,49 +1,32 @@
-import { ReactNode } from 'react';
-import { notFound } from 'next/navigation';
-import i18nConfig from '@/i18n/i18nConfig';
-import LayoutWrapper from '../layout-wrapper';
-import { ThemeProvider } from '@/components/theme-provider';
-import CookieConsent from '@/components/CookieConsent';
+import { ReactNode } from "react";
+import { notFound } from "next/navigation";
+import i18nConfig from "@/i18n/i18nConfig";
+import LayoutWrapper from "../layout-wrapper";
+import { ThemeProvider } from "@/components/theme-provider";
+import CookieConsent from "@/components/CookieConsent";
 
-export default async function LocaleLayout(
-  props: {
-    children: ReactNode;
-    params: Promise<{ locale: string }>;
-  }
-): Promise<ReactNode> {
+export default async function LocaleLayout(props: {
+  children: ReactNode;
+  params: Promise<{ locale: string }>;
+}) {
   const params = await props.params;
-
-  const {
-    children
-  } = props;
-
   const { locale } = params;
 
   if (!i18nConfig.locales.includes(locale)) {
     notFound();
   }
 
- return (
-    <html lang={locale} suppressHydrationWarning>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" crossOrigin="anonymous" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link rel="dns-prefetch" href="https://www.google-analytics.com" />
-        <link rel="dns-prefetch" href="https://www.googletagmanager.com" />
-      </head>
-      <body suppressHydrationWarning>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="light"
-          enableSystem={false}
-          disableTransitionOnChange
-        >
-          <LayoutWrapper locale={locale}>
-            {children}
-            <CookieConsent />
-          </LayoutWrapper>
-        </ThemeProvider>
-      </body>
-    </html>
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="light"
+      enableSystem={false}
+      disableTransitionOnChange
+    >
+      <LayoutWrapper locale={locale}>
+        {props.children}
+        <CookieConsent />
+      </LayoutWrapper>
+    </ThemeProvider>
   );
 }
