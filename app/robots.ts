d003ccd8522ca8fcfc3569712bb05588bc43fa95
Ahmed Nadeem <ahmed962456@gmail.com>
2026-07-03 11:29:44 -0700

    robot.txt2

---
+++ app/robots.ts
@@ -0,0 +1,16 @@
+﻿import type { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: [
+        '/.well-known/',
+        '/*?_rsc=',
+        '/job/',
+      ],
+    },
+    sitemap: 'https://www.itsolutionsworldwide.com/sitemap.xml',
+  };
+}
