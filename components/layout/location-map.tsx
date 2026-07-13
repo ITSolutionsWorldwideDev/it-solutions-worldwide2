07c1a1b1ff910e8a86b130508c8cdb52eb5e0a73
Ahmed Nadeem <ahmed962456@gmail.com>
2026-07-13 15:26:59 +0500

    contact map fixed

--- components/layout/location-map.tsx
+++ components/layout/location-map.tsx
@@ -7,7 +7,7 @@ export default function LocationMap() {
   const [shouldLoad, setShouldLoad] = useState(false);
 
   const mapSrc =
-    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2433.5289514228773!2d4.482805076926155!3d51.890982479691176!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c5cfd553bb1c9f%3A0xb340df3dc06d9ad0!2sMandenmakerstraat%20100C%2C%203194%20DG%20Hoogvliet%20Rotterdam%2C%20Netherlands!5e0!3m2!1sen!2s!4v1700000000000!5m2!1sen!2s";
+        "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2229.4664779419377!2d4.3753155!3d51.867517199999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c44b8a79991dfd%3A0xfeb452cbb689e588!2sIT%20Solutions%20Worldwide%20BV!5e1!3m2!1sen!2s!4v1782289665315!5m2!1sen!2s"
 
   useEffect(() => {
     if (!containerRef.current) return;
